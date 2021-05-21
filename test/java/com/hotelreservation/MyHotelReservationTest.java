f269327b088d07e50dba0664791e314974ebc8c3
prudhvi <prudhvieethakota123@gmail.com>
2021-05-21 11:06:11 +0530

    Added: Welcome Message To Hotel Reservation System

---
+++ test/java/com/hotelreservation/MyHotelReservationTest.java
@@ -0,0 +1,7 @@
+package com.hotelreservation;
+
+public class MyHotelReservationTest {
+    public static void main (String[] args) {
+        System.out.println("Welcome To Hotel Reservation System Program");
+    }
+}
